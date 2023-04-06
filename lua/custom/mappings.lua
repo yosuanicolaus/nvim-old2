f0e922ba2100abc53ace1451c95eb03dc746f520
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-04-07 09:45:37 +1000

    add and modify custom mapping

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,7 +3,16 @@ local M = {}
 
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<C-_>"] = { "<cmd> set hlsearch! <CR>", "toggle search highlight" },
+
+    -- for Neovide
+    ["<C-/>"] = { "<cmd> set hlsearch! <CR>", "toggle search highlight" },
+  },
+
+  i = {
+    ["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
+    ["<C-v>"] = { "<C-r>+", "paste" },
   },
 }
 
