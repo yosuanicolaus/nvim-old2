b76d86904b321dda7087229ac39692f4b1d3c0f6
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-04-07 12:39:06 +1000

    add Ctrl-a (select all) and Ctrl-enter (new line) mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,6 +5,7 @@ M.general = {
   n = {
     -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<C-_>"] = { "<cmd> set hlsearch! <CR>", "toggle search highlight" },
+    ["<C-a>"] = { "GVgg", "select all" },
 
     -- for Neovide
     ["<C-/>"] = { "<cmd> set hlsearch! <CR>", "toggle search highlight" },
@@ -13,6 +14,9 @@ M.general = {
   i = {
     ["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
     ["<C-v>"] = { "<C-r>+", "paste" },
+
+    -- for Neovide
+    ["<C-CR>"] = { "<End><CR>", "enter to new line" },
   },
 }
 
