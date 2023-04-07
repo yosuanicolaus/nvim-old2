8892d5f33c8ab78aa8520f7668f20f1c0d8e0091
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-04-08 09:40:32 +1000

    add more neovide config

--- lua/custom/neovide.lua
+++ lua/custom/neovide.lua
@@ -1,4 +1,9 @@
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
   vim.o.guifont = "JetBrainsMono Nerd Font:h11"
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_fullscreen = false
+  vim.g.neovide_cursor_animation_length = 0.07
+  vim.g.neovide_transparency = 0.95
+  vim.g.neovide_hide_mouse_when_typing = true
 end
