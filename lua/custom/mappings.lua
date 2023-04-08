37fbdfb16ba85c713eb5977bed1c805360170c8b
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-04-09 05:07:09 +1000

    clean mapping for kitty terminal & add <C-/> for comment shortcut

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,23 +3,32 @@ local M = {}
 
 M.general = {
   n = {
-    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<C-_>"] = { "<cmd> set hlsearch! <CR>", "toggle search highlight" },
     ["<C-a>"] = { "GVgg", "select all" },
-
-    -- for Neovide
-    ["<C-/>"] = { "<cmd> set hlsearch! <CR>", "toggle search highlight" },
+    ["<C-S-/>"] = { "<cmd> set hlsearch! <CR>", "toggle search highlight" },
   },
 
   i = {
     ["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
     ["<C-v>"] = { "<C-r>+", "paste" },
-
-    -- for Neovide
     ["<C-CR>"] = { "<End><CR>", "enter to new line" },
   },
 }
 
--- more keybinds!
+M.comment = {
+  n = {
+    ["<C-/>"] = {
+      function()
+        require("Comment.api").toggle.linewise.current()
+      end,
+      "toggle comment",
+    },
+  },
+  v = {
+    ["<C-/>"] = {
+      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+      "toggle comment",
+    },
+  },
+}
 
 return M
